1ba6e8c643e4f151bd2032d4c6dc50de9b5626b5
shubham kadam <1shubhamkadam1@gmail.com>
2022-01-12 20:23:42 +0530

    added collapsible search upto md devices

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,20 +18,50 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar fixed app>
+    <v-app-bar fixed app v-click-outside="onClickOutside">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title
+        v-if="!isSearch"
+        class="d-block d-md-none"
+        v-text="title"
+      />
+      <v-toolbar-title class="d-none d-md-block" v-text="title" />
       <v-spacer />
+      <v-btn
+        fab
+        v-if="!isSearch"
+        class="d-block d-md-none"
+        @click="isSearch = true"
+      >
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
       <v-autocomplete
         color="white"
-        class="mt-4"
+        class="mt-7 d-md-none"
+        v-if="isSearch"
         autofocus
+        clearable
         v-model="model"
         :items="searchItems"
         :loading="isLoading"
         :search-input.sync="search"
         placeholder="Start typing to Search"
-        prepend-icon="mdi-magnify"
+        prepend-inner-icon="mdi-magnify"
+        solo
+        @input="onSearchClick($event)"
+      ></v-autocomplete>
+      <v-autocomplete
+        color="white"
+        class="mt-7 d-none d-md-flex"
+        clearable
+        autofocus
+        v-model="model2"
+        :items="searchItems"
+        :loading="isLoading"
+        solo
+        :search-input.sync="search2"
+        placeholder="Start typing to Search"
+        prepend-inner-icon="mdi-magnify"
         @input="onSearchClick($event)"
       ></v-autocomplete>
     </v-app-bar>
@@ -54,8 +84,11 @@ export default {
       title: APP_NAME,
       searchItems: [],
       model: '',
+      model2: '',
       search: '',
+      search2: '',
       isLoading: false,
+      isSearch: false,
     }
   },
   watch: {
@@ -76,10 +109,35 @@ export default {
         value: slug,
       }))
     },
+    async search2(query) {
+      if (!query) {
+        this.articles = []
+        return
+      }
+
+      const items = await this.$content('songs')
+        .only(['title', 'slug'])
+        .sortBy('createdAt', 'asc')
+        .limit(12)
+        .search(query)
+        .fetch()
+      this.searchItems = items.map(({ title, slug }) => ({
+        text: title,
+        value: slug,
+      }))
+    },
   },
   methods: {
     onSearchClick(slug) {
-      this.$router.push({ path: `/songs/${slug}` })
+      if (slug) {
+        this.$router.push({ path: `/songs/${slug}` })
+      }
+    },
+    onClickOutside() {
+      console.log('outside')
+      if (this.isSearch) {
+        this.isSearch = false
+      }
     },
   },
 }
